7802680ce06b7384d7dbd315135d43339d795fb2
Will McCracken <will@wgmamm.com>
2023-10-20 15:19:37 -0400

    fix url for languages

--- languages.go
+++ languages.go
@@ -6,20 +6,18 @@ import (
 
 // Language struct
 type Language struct {
-	Languages []struct {
-		Iso6391     string `json:"iso_639_1"`
-		EnglishName string `json:"english_name"`
-		Name        string `json:"name"`
-	}
+	Iso6391     string `json:"iso_639_1"`
+	EnglishName string `json:"english_name"`
+	Name        string `json:"name"`
 }
 
 // GetLanguageList from configuration
 // https://developers.themoviedb.org/3/configuration/languages
-func (tmdb *TMDb) GetLanguageList() (*Language, error) {
-	var languageList Language
+func (tmdb *TMDb) GetLanguageList() ([]Language, error) {
+	var languageList []Language
 	uri := fmt.Sprintf("%s/configuration/languages?api_key=%s", baseURL, tmdb.apiKey)
 	fmt.Printf("%s", uri)
 	result, err := getTmdb(uri, &languageList)
 	fmt.Printf("%+v", result)
-	return result.(*Language), err
+	return result.([]Language), err
 }
