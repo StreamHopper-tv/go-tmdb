f8a68fd57287191829caf7f7a1b1585f6f8fbc47
Will McCracken <will@wgmamm.com>
2023-10-20 14:54:04 -0400

    fix return type

--- languages.go
+++ languages.go
@@ -13,9 +13,9 @@ type Language struct {
 
 // GetLanguageList from configuration
 // https://developers.themoviedb.org/3/configuration/languages
-func (tmdb *TMDb) GetLanguageList() ([]Language, error) {
+func (tmdb *TMDb) GetLanguageList() (*[]Language, error) {
 	var languageList []Language
 	uri := fmt.Sprintf("%s/configuration/languagest?api_key=%s", baseURL, tmdb.apiKey)
 	result, err := getTmdb(uri, &languageList)
-	return result.([]Language), err
+	return result.(*[]Language), err
 }
